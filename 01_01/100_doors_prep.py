77ebcd0c2f70c8304f07850694a6d4f19a980f55
raniprati <ranipratibha@gmail.com>
2023-03-24 18:22:27 +0530

    100 doors sol added

---
+++ 01_01/100_doors_prep.py
@@ -0,0 +1,11 @@
+doors = [False] * 101
+
+for j in range(1, 101):
+    for i in range(1, 101, j):
+        doors[j] = not doors[j]
+
+open = 0
+for i in range(1, 101):
+    if doors[i]:    open += 1
+
+print("No. of open doors = ", open)
