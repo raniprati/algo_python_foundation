20908fd389890a40d105351bbb1e5a341b24011c
raniprati <128400192+raniprati@users.noreply.github.com>
2023-03-24 18:33:12 +0530

    Update 100_doors_prep.py

--- 01_01/100_doors_prep.py
+++ 01_01/100_doors_prep.py
@@ -6,6 +6,8 @@ for j in range(1, 101):
 
 open = 0
 for i in range(1, 101):
-    if doors[i]:    open += 1
+    if doors[i]:
+        print("Open door: ", i)
+        open += 1
 
 print("No. of open doors = ", open)
